22ef32437cdcaa4c6f107dfe21bef8ae29efd5fd
nicolaswiedel <nicolaswiedel@googlemail.com>
2023-11-17 16:48:17 +0100

    Zoom korriegiret und Hudelemente zugefügt

--- Dragoncraft/Assets/Scripts/Camera/CameraController.cs
+++ Dragoncraft/Assets/Scripts/Camera/CameraController.cs
@@ -16,7 +16,7 @@ namespace Dragoncraft
         [SerializeField]
         private float _scrollSpeed = 1000f;
         [SerializeField]
-        private Vector2 _scrollLimit = new Vector2(5f, 10f);
+        private Vector2 _scrollLimit = new Vector2(5f, 25f);
         private Vector3 _initialPosition = Vector3.zero;
         private Camera _camera = null;
 
@@ -36,7 +36,7 @@ namespace Dragoncraft
         {
             float scroll = Input.GetAxis("Mouse ScrollWheel");
             scroll = scroll * _scrollSpeed * Time.deltaTime;
-            _camera.orthographicSize += scroll;
+            _camera.orthographicSize -= scroll;
             _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize, 
                 _scrollLimit.x, _scrollLimit.y);
         }
